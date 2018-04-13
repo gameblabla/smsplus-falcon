f1d4b5cd09e4642cbd4d4d13217f6ea66a6febc0
gameblabla <gameblabla@openmailbox.org>
2018-04-13 02:08:25 -0400

    Fix broken build, change opt

--- src/cm.bat
+++ src/cm.bat
@@ -1,1 +1,1 @@
-"C:\VBCC\bin\vc.exe" +C:\VBCC\config\normal.pc -cpu=68030 -O3 -DMSB_FIRST -o sms_030.prg emu_main.c saves.c falcon.c render.c sms.c system.c vdp.c z80.c falcsys.c GG.S falcsysa.s 256192.S SMSRGB.S GGRGB.S
+"C:\VBCC\bin\vc.exe" +C:\VBCC\config\normal.pc -cpu=68030 -O2 -DMSB_FIRST -o sms_030.prg emu_main.c saves.c falcon.c render.c sms.c system.c vdp.c z80.c falcsys.c GG.S falcsysa.s 256192.S SMSRGB.S GGRGB.S
--- src/cm_60.bat
+++ src/cm_60.bat
@@ -1,1 +1,1 @@
-"C:\VBCC\bin\vc.exe" +C:\VBCC\config\normal.pc -cpu=68040 -O3 -DMSB_FIRST -DCTSIXTY -o sms_ct60.prg emu_main.c saves.c falcon.c render.c sms.c system.c vdp.c z80.c falcsys.c GG.S falcsysa.s 256192.S  SMSRGB.S GGRGB.S
+"C:\VBCC\bin\vc.exe" +C:\VBCC\config\normal.pc -cpu=68040 -O2 -DMSB_FIRST -DCTSIXTY -o sms_ct60.prg emu_main.c saves.c falcon.c render.c sms.c system.c vdp.c z80.c falcsys.c GG.S falcsysa.s 256192.S  SMSRGB.S GGRGB.S
